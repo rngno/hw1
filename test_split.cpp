f739fbe236ae6850687f853eadfba84e138c520d
MJ Deus <mdeus@usc.edu>
2025-09-12 23:23:10 +0000

    recursive linked list done yay

--- test_split.cpp
+++ test_split.cpp
@@ -10,8 +10,23 @@ g++ split.cpp test_split.cpp -o test_split
 */
 
 #include "split.h"
+#include <iostream> // i want this lol 
 
 int main(int argc, char* argv[])
 {
-
+  Node* in2 = new Node(2, nullptr);
+  Node* in = new Node(1, in2);
+  Node* odds;
+  Node* evens;
+  std::cout << "Testing small sized linked list, only 1 odd and 1 even:" << std::endl;
+  split(in, odds, evens);
+  if(in == nullptr){
+    std::cout << "Passed 1 odd 1 even test" << std::endl;
+  }
+  else{
+    std::cout << "Failed 1 odd 1 even test" << std::endl;
+  }
+  delete in;
+  delete in2;
+  return 0;
 }
